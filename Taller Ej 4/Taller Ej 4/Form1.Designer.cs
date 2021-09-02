740a89390e4d72ff626b5f58cdda0a0afaeaa7b9
Xavier Perez <perezxavier918@gmail.com>
2021-09-02 05:58:05 -0600

    POO_Taller1_P4_Interfaz

--- Taller Ej 4/Taller Ej 4/Form1.Designer.cs
+++ Taller Ej 4/Taller Ej 4/Form1.Designer.cs
@@ -28,13 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txb_NE1 = new System.Windows.Forms.TextBox();
+            this.txb_NE2 = new System.Windows.Forms.TextBox();
+            this.txb_NE3 = new System.Windows.Forms.TextBox();
+            this.txb_AE3 = new System.Windows.Forms.TextBox();
+            this.txb_AE2 = new System.Windows.Forms.TextBox();
+            this.txb_AE1 = new System.Windows.Forms.TextBox();
+            this.cmb_E1 = new System.Windows.Forms.ComboBox();
+            this.cmb_E2 = new System.Windows.Forms.ComboBox();
+            this.cmb_E3 = new System.Windows.Forms.ComboBox();
+            this.txb_HE3 = new System.Windows.Forms.TextBox();
+            this.txb_HE2 = new System.Windows.Forms.TextBox();
+            this.txb_HE1 = new System.Windows.Forms.TextBox();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txb_NE1
+            // 
+            this.txb_NE1.Location = new System.Drawing.Point(31, 112);
+            this.txb_NE1.Name = "txb_NE1";
+            this.txb_NE1.Size = new System.Drawing.Size(147, 20);
+            this.txb_NE1.TabIndex = 0;
+            // 
+            // txb_NE2
+            // 
+            this.txb_NE2.Location = new System.Drawing.Point(31, 148);
+            this.txb_NE2.Name = "txb_NE2";
+            this.txb_NE2.Size = new System.Drawing.Size(147, 20);
+            this.txb_NE2.TabIndex = 1;
+            // 
+            // txb_NE3
+            // 
+            this.txb_NE3.Location = new System.Drawing.Point(31, 189);
+            this.txb_NE3.Name = "txb_NE3";
+            this.txb_NE3.Size = new System.Drawing.Size(147, 20);
+            this.txb_NE3.TabIndex = 2;
+            // 
+            // txb_AE3
+            // 
+            this.txb_AE3.Location = new System.Drawing.Point(205, 189);
+            this.txb_AE3.Name = "txb_AE3";
+            this.txb_AE3.Size = new System.Drawing.Size(147, 20);
+            this.txb_AE3.TabIndex = 5;
+            // 
+            // txb_AE2
+            // 
+            this.txb_AE2.Location = new System.Drawing.Point(205, 148);
+            this.txb_AE2.Name = "txb_AE2";
+            this.txb_AE2.Size = new System.Drawing.Size(147, 20);
+            this.txb_AE2.TabIndex = 4;
+            // 
+            // txb_AE1
+            // 
+            this.txb_AE1.Location = new System.Drawing.Point(205, 112);
+            this.txb_AE1.Name = "txb_AE1";
+            this.txb_AE1.Size = new System.Drawing.Size(147, 20);
+            this.txb_AE1.TabIndex = 3;
+            this.txb_AE1.TextChanged += new System.EventHandler(this.txb_AE1_TextChanged);
+            // 
+            // cmb_E1
+            // 
+            this.cmb_E1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_E1.FormattingEnabled = true;
+            this.cmb_E1.Location = new System.Drawing.Point(376, 110);
+            this.cmb_E1.Name = "cmb_E1";
+            this.cmb_E1.Size = new System.Drawing.Size(121, 21);
+            this.cmb_E1.TabIndex = 6;
+            // 
+            // cmb_E2
+            // 
+            this.cmb_E2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_E2.FormattingEnabled = true;
+            this.cmb_E2.Location = new System.Drawing.Point(376, 148);
+            this.cmb_E2.Name = "cmb_E2";
+            this.cmb_E2.Size = new System.Drawing.Size(121, 21);
+            this.cmb_E2.TabIndex = 7;
+            // 
+            // cmb_E3
+            // 
+            this.cmb_E3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_E3.FormattingEnabled = true;
+            this.cmb_E3.Location = new System.Drawing.Point(376, 189);
+            this.cmb_E3.Name = "cmb_E3";
+            this.cmb_E3.Size = new System.Drawing.Size(121, 21);
+            this.cmb_E3.TabIndex = 8;
+            // 
+            // txb_HE3
+            // 
+            this.txb_HE3.Location = new System.Drawing.Point(521, 191);
+            this.txb_HE3.Name = "txb_HE3";
+            this.txb_HE3.Size = new System.Drawing.Size(49, 20);
+            this.txb_HE3.TabIndex = 9;
+            // 
+            // txb_HE2
+            // 
+            this.txb_HE2.Location = new System.Drawing.Point(521, 149);
+            this.txb_HE2.Name = "txb_HE2";
+            this.txb_HE2.Size = new System.Drawing.Size(49, 20);
+            this.txb_HE2.TabIndex = 10;
+            // 
+            // txb_HE1
+            // 
+            this.txb_HE1.Location = new System.Drawing.Point(521, 112);
+            this.txb_HE1.Name = "txb_HE1";
+            this.txb_HE1.Size = new System.Drawing.Size(49, 20);
+            this.txb_HE1.TabIndex = 11;
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.Location = new System.Drawing.Point(331, 317);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_Calcular.TabIndex = 12;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(373, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Cargo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(518, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Horas trabajadas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(782, 352);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.txb_HE1);
+            this.Controls.Add(this.txb_HE2);
+            this.Controls.Add(this.txb_HE3);
+            this.Controls.Add(this.cmb_E3);
+            this.Controls.Add(this.cmb_E2);
+            this.Controls.Add(this.cmb_E1);
+            this.Controls.Add(this.txb_AE3);
+            this.Controls.Add(this.txb_AE2);
+            this.Controls.Add(this.txb_AE1);
+            this.Controls.Add(this.txb_NE3);
+            this.Controls.Add(this.txb_NE2);
+            this.Controls.Add(this.txb_NE1);
+            this.Name = "Form1";
+            this.Text = "Sueldo de empleados";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txb_NE1;
+        private System.Windows.Forms.TextBox txb_NE2;
+        private System.Windows.Forms.TextBox txb_NE3;
+        private System.Windows.Forms.TextBox txb_AE3;
+        private System.Windows.Forms.TextBox txb_AE2;
+        private System.Windows.Forms.TextBox txb_AE1;
+        private System.Windows.Forms.ComboBox cmb_E1;
+        private System.Windows.Forms.ComboBox cmb_E2;
+        private System.Windows.Forms.ComboBox cmb_E3;
+        private System.Windows.Forms.TextBox txb_HE3;
+        private System.Windows.Forms.TextBox txb_HE2;
+        private System.Windows.Forms.TextBox txb_HE1;
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Taller Ej 4/Taller Ej 4/Form1.cs
+++ Taller Ej 4/Taller Ej 4/Form1.cs
@@ -16,5 +16,30 @@ namespace Taller_Ej_4
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmb_E1.Items.Clear();
+            cmb_E2.Items.Clear();
+            cmb_E3.Items.Clear();
+
+            cmb_E1.Items.Add("Gerente");
+            cmb_E1.Items.Add("Asistente");
+            cmb_E1.Items.Add("Secretaria");
+
+            cmb_E2.Items.Add("Gerente");
+            cmb_E2.Items.Add("Asistente");
+            cmb_E2.Items.Add("Secretaria");
+
+            cmb_E3.Items.Add("Gerente");
+            cmb_E3.Items.Add("Asistente");
+            cmb_E3.Items.Add("Secretaria");
+
+        }
+
+        private void txb_AE1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
