a04088e2dd1384f1a073fcc5f2b2471be3934873
Xavier Perez <perezxavier918@gmail.com>
2021-09-02 07:47:52 -0600

    POO_Taller1_P4_Aplicacion final

--- Taller Ej 4/Taller Ej 4/Form1.Designer.cs
+++ Taller Ej 4/Taller Ej 4/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txb_NE1
@@ -184,11 +185,22 @@
             this.label4.TabIndex = 16;
             this.label4.Text = "Horas trabajadas";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(314, 24);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Por favor llene todos los campos";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 352);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -233,6 +245,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Taller Ej 4/Taller Ej 4/Form1.cs
+++ Taller Ej 4/Taller Ej 4/Form1.cs
@@ -31,7 +31,7 @@ namespace Taller_Ej_4
             cmb_E2.Items.Add("Gerente");
             cmb_E2.Items.Add("Asistente");
             cmb_E2.Items.Add("Secretaria");
-            cmb_E1.Items.Add("Otro");
+            cmb_E2.Items.Add("Otro");
 
             cmb_E3.Items.Add("Gerente");
             cmb_E3.Items.Add("Asistente");
@@ -51,10 +51,102 @@ namespace Taller_Ej_4
             try
             {
                 int horas1 = Convert.ToInt32(txb_HE1.Text), horas2 = Convert.ToInt32(txb_HE2.Text), horas3 = Convert.ToInt32(txb_HE3.Text);
-                double sb1=0, sb3 = 0, sb2 = 0, isss =0.00525, afp=0.00688, renta=0.01, sl1=0, sl2 = 0, sl3 = 0, bono1 =0, bono2=0, bono3=0;
+                double sb1 = 0, sb3 = 0, sb2 = 0, isss = 0.00525, afp = 0.00688, renta = 0.01, sl1 = 0, sl2 = 0, sl3 = 0, bono1 = 0, bono2 = 0, bono3 = 0, sln1 = 0, sln2 = 0, sln3 = 0;
                 string Ne1 = txb_NE1.Text, Ne2 = txb_NE2.Text, Ne3 = txb_NE3.Text, Ae1 = txb_AE1.Text, Ae2 = txb_AE2.Text, Ae3 = txb_AE3.Text;
 
                 if (cmb_E1.SelectedItem.ToString() == "Gerente" && cmb_E2.SelectedItem.ToString() == "Asistente" && cmb_E3.SelectedItem.ToString() == "Secretaria")
+                {
+                    if (Convert.ToInt32(txb_HE1.Text) > 0 && Convert.ToInt32(txb_HE2.Text) > 0 && Convert.ToInt32(txb_HE3.Text) > 0)
+                    {
+                        int i, o, p;
+                        for (i = 1; i <= horas1; i++)
+                        {
+                            if (i <= 160)
+                            {
+                                sb1 = sb1 + 9.75;
+                            }
+                            if (i > 160)
+                            {
+                                sb1 = sb1 + 11.50;
+                            }
+                        }
+
+                        for (o = 1; o <= horas2; o++)
+                        {
+                            if (o <= 160)
+                            {
+                                sb2 = sb2 + 9.75;
+                            }
+                            if (o > 160)
+                            {
+                                sb2 = sb2 + 11.50;
+                            }
+                        }
+
+                        for (p = 1; p <= horas3; p++)
+                        {
+                            if (p <= 160)
+                            {
+                                sb3 = sb3 + 9.75;
+                            }
+                            if (p > 160)
+                            {
+                                sb3 = sb3 + 11.50;
+                            }
+                        }
+
+
+                        sln1 = ((-isss * sb1) - (afp * sb1) - (renta * sb1) + sb1);
+
+
+                        sln3 = ((-isss * sb3) - (afp * sb3) - (renta * sb3) + sb3);
+
+
+                        sln2 = ((-isss * sb2) - (afp * sb2) - (renta * sb2) + sb2);
+
+                        MessageBox.Show("No hay bono");
+                        MessageBox.Show("El empleado " + Ne1 + " " + Ae1 + " con sueldo bruto de $" + sb1 + " con descuento de afp $" + Math.Round(afp * sb1, 2) + " ISSS $" + Math.Round(isss * sb1, 2) + " y renta $" + Math.Round(renta * sb1, 2) + " tiene un sueldo liquido de $" + Math.Round(sln1, 2) + "");
+                        MessageBox.Show("El empleado " + Ne2 + " " + Ae2 + " con sueldo bruto de $" + sb2 + " con descuento de afp $" + Math.Round(afp * sb2, 2) + " ISSS $" + Math.Round(isss * sb2, 2) + " y renta $" + Math.Round(renta * sb2, 2) + " tiene un sueldo liquido de $" + Math.Round(sln2, 2) + "");
+                        MessageBox.Show("El empleado " + Ne3 + " " + Ae3 + " con sueldo bruto de $" + sb3 + " con descuento de afp $" + Math.Round(afp * sb3, 2) + " ISSS $" + Math.Round(isss * sb3, 2) + " y renta $" + Math.Round(renta * sb3, 2) + " tiene un sueldo liquido de $" + Math.Round(sln3, 2) + "");
+
+                        if (sln1 < sln2)
+                        {
+                            if (sln2 < sln3)
+                            {
+                                MessageBox.Show("" + Ne3 + " " + Ae3 + " tiene el sueldo liquido mas alto");
+                            }
+                            else
+                                MessageBox.Show("" + Ne2 + " " + Ae2 + " tiene el sueldo liquido mas alto");
+                        }
+                        else if (sln1 > sln2)
+                        {
+                            if (sln1 < sln3)
+                            {
+                                MessageBox.Show("" + Ne3 + " " + Ae3 + " tiene el sueldo liquido mas alto");
+                            }
+                            else
+                                MessageBox.Show("" + Ne1 + " " + Ae1 + " tiene el sueldo liquido mas alto");
+                        }
+
+                        if (sl1 < sl2 && sl1 < sl3)
+                        {
+                            MessageBox.Show("" + Ne1 + " " + Ae1 + " tiene el sueldo liquido mas bajo");
+                        }
+                        else
+                        {
+                            if (sl2 < sl1 && sl2 < sl3)
+                            {
+                                MessageBox.Show("" + Ne2 + " " + Ae2 + " tiene el sueldo liquido mas bajo");
+                            }
+                            else
+                            {
+                                MessageBox.Show("" + Ne3 + " " + Ae3 + " tiene el sueldo liquido mas bajo");
+                            }
+                        }
+
+                    }
+                }
+                else
                 {
                     if (Convert.ToInt32(txb_HE1.Text) > 0 && Convert.ToInt32(txb_HE2.Text) > 0 && Convert.ToInt32(txb_HE3.Text) > 0)
                     {
@@ -99,77 +191,54 @@ namespace Taller_Ej_4
                         bonon2(bono2);
                         bonon3(bono3);
 
-                        sl1 = (-((isss * sb1) + (afp * sb1) + (renta * sl1)) + sb1);
-                        
+                        sl1 = (-((isss * sb1) + (afp * sb1) + (renta * sb1)) + sb1);
+                        sl1 = Math.Round((sl1 * bono1) + sl1, 2);
 
-                        sl3 = (-((isss * sb3) + (afp * sb3) + (renta * sl3)) + sb3);
-                        
+                        sl3 = (-((isss * sb3) + (afp * sb3) + (renta * sb3)) + sb3);
+                        sl3 = Math.Round((sl3 * bono1) + sl3, 2);
 
-                        sl2 = (-((isss * sb2) + (afp * sb2) + (renta * sl2)) + sb1);
+                        sl2 = (-((isss * sb2) + (afp * sb2) + (renta * sb2)) + sb2);
+                        sl2 = Math.Round((sl2 * bono1) + sl2, 2);
 
-                        MessageBox.Show("No hay bono");
                         MessageBox.Show("El empleado " + Ne1 + " " + Ae1 + " con sueldo bruto de $" + sb1 + " con descuento de afp $" + Math.Round(afp * sb1, 2) + " ISSS $" + Math.Round(isss * sb1, 2) + " y renta $" + Math.Round(renta * sl1, 2) + " tiene un sueldo liquido de $" + Math.Round(sl1, 2) + "");
                         MessageBox.Show("El empleado " + Ne2 + " " + Ae2 + " con sueldo bruto de $" + sb2 + " con descuento de afp $" + Math.Round(afp * sb2, 2) + " ISSS $" + Math.Round(isss * sb2, 2) + " y renta $" + Math.Round(renta * sl2, 2) + " tiene un sueldo liquido de $" + Math.Round(sl2, 2) + "");
                         MessageBox.Show("El empleado " + Ne3 + " " + Ae3 + " con sueldo bruto de $" + sb3 + " con descuento de afp $" + Math.Round(afp * sb3, 2) + " ISSS $" + Math.Round(isss * sb3, 2) + " y renta $" + Math.Round(renta * sl3, 2) + " tiene un sueldo liquido de $" + Math.Round(sl3, 2) + "");
-                    }
-                }
-                else
-                {
-                    if (Convert.ToInt32(txb_HE1.Text) > 0 && Convert.ToInt32(txb_HE2.Text) > 0 && Convert.ToInt32(txb_HE3.Text) > 0)
-                    {
-                        int i,o,p;
-                        for (i = 1; i<= horas1; i++)
+
+                        if (sl1 < sl2)
                         {
-                            if (i <= 160)
+                            if (sl2 < sl3)
                             {
-                                sb1 = sb1 + 9.75;
+                                MessageBox.Show("" + Ne3 + " " + Ae3 + " tiene el sueldo liquido mas alto");
                             }
-                            if (i > 160)
+                            else
+                                MessageBox.Show("" + Ne2 + " " + Ae2 + " tiene el sueldo liquido mas alto");
+                        }
+                        else if (sl1 > sl2)
+                        {
+                            if (sl1 < sl3)
                             {
-                                sb1 = sb1 + 11.50;
+                                MessageBox.Show("" + Ne3 + " " + Ae3 + " tiene el sueldo liquido mas alto");
+                            }
+                            else
+                                MessageBox.Show("" + Ne1 + " " + Ae1 + " tiene el sueldo liquido mas alto");
+                        }
+
+                        if (sl1 < sl2 && sl1 < sl3)
+                        {
+                            MessageBox.Show("" + Ne1 + " " + Ae1 + " tiene el Sueldo liquido mas bajo");
+                        }
+                        else
+                        {
+                            if (sl2 < sl1 && sl2 < sl3)
+                            {
+                                MessageBox.Show("" + Ne2 + " " + Ae2 + " tiene el sueldo liquido mas bajo");
+                            }
+                            else
+                            {
+                                MessageBox.Show("" + Ne3 + " " + Ae3 + " tiene el sueldo liquido mas bajo");
                             }
                         }
 
-                        for (o = 1; o <= horas2; o++)
-                        {
-                            if (o <= 160)
-                            {
-                                sb2 = sb2 + 9.75;
-                            }
-                            if (o > 160)
-                            {
-                                sb2 = sb2 + 11.50;
-                            }
-                        }
-
-                        for (p = 1; p <= horas3; p++)
-                        {
-                            if (p <= 160)
-                            {
-                                sb3 = sb3 + 9.75;
-                            }
-                            if (p > 160)
-                            {
-                                sb3 = sb3 + 11.50;
-                            }
-                        }
-
-                        bonon1(bono1);
-                        bonon2(bono2);
-                        bonon3(bono3);
-
-                        sl1 = (-((isss * sb1) + (afp * sb1) + (renta * sl1)) + sb1);
-                        sl1 = Math.Round((sl1 * bono1) + sl1, 2);
-
-                        sl3 = (-((isss * sb3) + (afp * sb3) + (renta * sl3)) + sb3);
-                        sl3 = Math.Round((sl3 * bono1) + sl3, 2);
-
-                        sl2 = (-((isss * sb2) + (afp * sb2) + (renta * sl2)) + sb1);
-                        sl2 = Math.Round((sl2 * bono1) + sl2, 2);
-
-                        MessageBox.Show("El empleado "+Ne1+" "+Ae1+" con sueldo bruto de $"+sb1+" con descuento de afp $"+ Math.Round(afp * sb1,2) + " ISSS $"+ Math.Round(isss * sb1,2) + " y renta $"+ Math.Round(renta * sl1,2) + " tiene un sueldo liquido de $"+ Math.Round(sl1,2) + "");
-                        MessageBox.Show("El empleado " + Ne2 + " " + Ae2 + " con sueldo bruto de $" + sb2 + " con descuento de afp $" + Math.Round(afp * sb2, 2) + " ISSS $" + Math.Round(isss * sb2, 2) + " y renta $" + Math.Round(renta * sl2, 2) + " tiene un sueldo liquido de $" + Math.Round(sl2, 2) + "");
-                        MessageBox.Show("El empleado " + Ne3 + " " + Ae3 + " con sueldo bruto de $" + sb3 + " con descuento de afp $" + Math.Round(afp * sb3, 2) + " ISSS $" + Math.Round(isss * sb3, 2) + " y renta $" + Math.Round(renta * sl3, 2) + " tiene un sueldo liquido de $" + Math.Round(sl3, 2) + "");
                     }
                 }
             }
